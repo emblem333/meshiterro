df4074fac113b3ee1339a719c48af96d6ff628eb
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-02-15 05:58:03 +0000

    [Add]4-18

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,7 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
 
   def get_image
     if image.attached?
@@ -9,7 +10,7 @@ class PostImage < ApplicationRecord
       'no_image.jpg'
     end
   end
-  
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
